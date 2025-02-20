05d8fd6615b5b8d195004396e3aa2c0db1985c9d
YupiSoftwarePunk <youngperchik2404@gmail.com>
2025-02-20 13:53:12 +0500

    Added files from college

---
+++ 19.02/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Figure
+    {
+        public static double V(double a)
+        {
+            return a * a * a;
+        }
+
+        public static double V(double a, double b, double c) 
+        {
+            return a * b * c;
+        }
+    }
+}
---
+++ 19.02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Перегрузка операторов
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Figure.V(12.2));
+            Console.WriteLine(Figure.V(12.22, 34.11, 99.6));
+
+            Console.WriteLine(Number.PrintMax(12, 10));
+        }
+    }
+}
